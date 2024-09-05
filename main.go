9934b588f63b38d25a15e800f619e4d20ca1b3b5
verna0214 <yuhsichen0214@gmail.com>
2024-09-05 22:16:38 +0800

    feat: create Movie & Director struct

--- main.go
+++ main.go
@@ -10,3 +10,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
